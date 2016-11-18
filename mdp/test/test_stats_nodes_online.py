e1125d98a854c7b679658280e2b0b11eeec3d1da
Varun Kompella <varunrajk@gmail.com>
2016-11-18 12:46:48 +0100

    Added test functions for all stats online nodes

--- mdp/test/test_stats_nodes_online.py
+++ mdp/test/test_stats_nodes_online.py
@@ -1,6 +1,13 @@
 
-from mdp.nodes import MovingTimeDiffNode
+from mdp.nodes import MovingAvgNode, MovingTimeDiffNode
 from ._tools import *
+
+
+def test_moving_avg_node():
+    node = MovingAvgNode()
+    x = mdp.numx_rand.randn(1000, 5) + mdp.numx_rand.uniform(-3,3,5)
+    node.train(x)
+    assert_array_almost_equal(node.avg[0], x.mean(axis=0))
 
 def test_movingtimediffnode():
     node = MovingTimeDiffNode()
