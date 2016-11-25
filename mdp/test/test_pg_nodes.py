377498e5b32175a49307249f927625614e18625a
Varun Kompella <varunrajk@gmail.com>
2016-11-25 15:06:09 +0100

    Add skip condition to the test function.

--- mdp/test/test_pg_nodes.py
+++ mdp/test/test_pg_nodes.py
@@ -3,10 +3,12 @@ from _tools import *
 from mdp.nodes import PG2DNode, PGCurveNode, PGImageNode
 import os
 
+requires_pyqtgraph = skip_on_condition("not mdp.config.has_pyqtgraph","This test requires PyQtGraph")
 
+@requires_pyqtgraph
 def test_pg_nodes():
     if os.environ.has_key('DISPLAY')  and (os.environ['DISPLAY'] != ''):
-        for dispnode in [PG2DNode(), PGCurveNode(), PGImageNode(img_xy=(10,10))]:
+        for dispnode in [PG2DNode(), PGCurveNode(), PGImageNode(img_shape=(10,10))]:
             x = mdp.numx_rand.randn(1 ,100)
             y = dispnode(x)
             assert dispnode._viewer.is_alive()
